3c69131fe3503f8e9018303a0deada7ccd5ea841
Jeff Yu <jeff.yu@sparkventures.co.nz>
2016-09-28 17:00:44 +1300

    Implement all charts, need more changes

--- public/js/App.js
+++ public/js/App.js
@@ -8,17 +8,49 @@ App.config(['$routeProvider', function($routeProvider) {
         templateUrl: '/views/home.html',
         controller: 'HomeController'
     }).
-   /* when('/time-management', {
-     templateUrl: '/views/time-management',
-     controller: 'TimeManagementController',
-    /!* resolve: {
-     session: setSession
-     }*!/
-     }).*/
+    when('/time-management', {
+     templateUrl: '/views/time-management.html',
+     controller: 'OthersController'
+    }).
+    when('/inbox', {
+        templateUrl: '/views/inbox.html',
+        controller: 'OthersController'
+    }).
+    when('/calendar', {
+        templateUrl: '/views/calendar.html',
+        controller: 'OthersController'
+    }).
+    when('/analytics', {
+        templateUrl: '/views/analytics.html',
+        controller: 'OthersController'
+    }).
+    when('/settings', {
+        templateUrl: '/views/settings.html',
+        controller: 'OthersController'
+    }).
     otherwise({
         redirectTo: '/'
     });
+
 }]);
 
+App.run(['$route', '$rootScope', '$location', '$window', function ($route, $rootScope, $location, $window) {
+    var original = $location.path;
+    $location.path = function (path, reload) {
+        if (reload === false) {
+            var lastRoute = $route.current;
+            var un = $rootScope.$on('$locationChangeSuccess', function () {
+                $route.current = lastRoute;
+                un();
+            });
+        }
+        return original.apply($location, [path]);
+    };
+
+    $rootScope.$on('$routeChangeSuccess', function(event) {
+        $rootScope.activeRoute = $location.path();
+    });
+
+}]);
 
 
--- public/js/controllers/Controllers.js
+++ public/js/controllers/Controllers.js
@@ -68,4 +68,111 @@ Controllers.controller('HomeController', function($scope, $http, $location) {
         }
     };
 
+    $scope.labels2 = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "111"];
+    $scope.data2 = [300, 500, 100, 50];
+    $scope.colors2 = ["#83e3ff", "#00afd9", "#2dc0e8", "#53cff3"];
+    $scope.options2 = {
+        cutoutPercentage: 50
+    }
+
+    $scope.data3 = [[20, 28, 29, 22, 20, 10, 6]];
+    $scope.labels3 = ["S","M","T","W","T","F","S"];
+    $scope.colors3= [{
+        backgroundColor: '#ff4c79',
+        fill:false,
+        tension: 0
+    }];
+    $scope.options3 = {
+        scales: {
+            fill: false,
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true   // minimum value will be 0.
+                }
+            }],
+            xAxes: [{
+                gridLines : {
+                    display : false
+                }
+            }]
+        }
+    };
+
+    $scope.labels4 = ["30.05", "31.05", "01.06", "02.06", "03.06","04.06","05.06","06.06","07.06","08.06","09.06"];
+    $scope.data4 = [
+        [0, 30, 20, 42, 56, 39, 30,20,10,20,0],
+        [0, 42, 45, 26, 35, 22, 42, 13, 32, 24, 0]
+    ];
+    $scope.colors4 = [
+        {
+            backgroundColor: 'rgba(83,200,235,0.8)',
+            pointRadius: 0,
+            //fill: false,
+            tension: 0
+        },
+        {
+            backgroundColor: 'rgba(195,226,244,0.8)',
+            pointRadius: 0,
+            //fill: false,
+            tension: 0
+        }
+    ]
+    $scope.options4 = {
+        scales: {
+            fill: false,
+            yAxes: [{
+                display:false,
+                ticks: {
+                    beginAtZero: true   // minimum value will be 0.
+                },
+                gridLines : {
+                    display : false
+                }
+            }],
+            xAxes: [{
+                display:false,
+                gridLines : {
+                    display : false
+                }
+            }]
+        }
+    };
+
+
+    $scope.labels5 = ["30.05", "31.05", "01.06", "02.06", "03.06","04.06","05.06","06.06","07.06","08.06","09.06"];
+    $scope.data5 = [
+        [0, 30, 20, 42, 56, 39, 30,20,10,20,0]
+    ];
+    $scope.colors5 = [
+        {
+            borderColor: 'rgba(83,200,235,0.8)',
+            pointRadius: 0,
+            fill: false,
+            tension: 0
+        }
+    ]
+    $scope.options5 = {
+        scales: {
+            fill: false,
+            yAxes: [{
+                display:false,
+                ticks: {
+                    beginAtZero: true   // minimum value will be 0.
+                },
+                gridLines : {
+                    display : false
+                }
+            }],
+            xAxes: [{
+                display:false,
+                gridLines : {
+                    display : false
+                }
+            }]
+        }
+    };
+});
+
+Controllers.controller('OthersController', function($scope) {
+    //other stuff
 });
